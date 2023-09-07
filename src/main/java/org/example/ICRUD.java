64ac8476fa29de9893e131a26098e331bbdc723b
eunsaemsaem <sweeet0308@gmail.com>
2023-09-07 23:36:31 +0900

    change ICRUD

--- src/main/java/org/example/ICRUD.java
+++ src/main/java/org/example/ICRUD.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public interface ICRUD {
+
+    public Object add(); //데이터 추가 시 사용
+                         // 해당 추가된 객체를 return하기 위해 Object 사용
+    public int update (Object obj); //데이터 수정
+    public int delete (Object obj); //데이터 삭제
+    public void selectOne (int id); //데이터 1개 조회
+
 }
