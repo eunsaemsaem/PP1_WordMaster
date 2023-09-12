54cb7a8d326d4b117398c7d1a9e1e979400898b8
eunsaemsaem <sweeet0308@gmail.com>
2023-09-12 19:37:51 +0900

    change listAll, deleteItem, updateItem

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class WordCRUD implements ICRUD{
+public class WordCRUD implements ICRUD { //파일 작성 > 파일 로드 > 수정, 삭제
 
     ArrayList<Word> list;
     Scanner s;
@@ -15,45 +19,148 @@ public class WordCRUD implements ICRUD{
 
     @Override
     public Object add() {
-        System.out.println ("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        System.out.println("=> 난이도(1,2,3) & 새 단어 입력 : ");
         int level = s.nextInt();
         String word = s.nextLine();
 
-        System.out.println ("뜻 입력 : ");
+        System.out.println("=> 뜻 입력 : ");
         String meaning = s.nextLine();
 
-        return new Word (0, level, word, meaning);
+        return new Word(0, level, word, meaning);
     }
 
     public void addWord() {
 
-        Word one = (Word)add();
+        Word one = (Word) add();
         list.add(one);
 
-        System.out.println ("새 단어가 단어장에 추가되었습니다.");
+        System.out.println("새 단어가 단어장에 추가되었습니다.");
 
     }
 
-    @Override
-    public  int update (Object obj) {return 0;}
+    public void listAll() {
 
-    @Override
-    public int delete (Object obj) {return 0;}
-
-    @Override
-    public  void selectOne (int id) {}
-
-    public void listAll () {
-
-        System.out.println ("------------------------");
+        System.out.println("------------------------");
 
         for (int i = 0; i < list.size(); i++) {
-            System.out.print ((i+1) + " ");
-            System.out.println (list.get(i).toString());
+            System.out.print((i + 1) + " ");
+            System.out.println(list.get(i).toString());
         }
 
         System.out.println("--------------------------");
 
     }
 
+    public ArrayList<Integer> listAll(String keyword) {
+
+        ArrayList<Integer> idlist = new ArrayList<>(); //새로운 list 생성
+
+        System.out.println("------------------------");
+        for (int i = 0; i < list.size(); i++) {
+
+            int j = 0;
+            String word = list.get(i).getWord();
+
+            if (!word.contains(keyword)) { //단어가 keyword를 포함하지 않으면
+                continue;
+            }
+
+            System.out.print((j + 1) + " ");
+            System.out.println(list.get(i).toString());
+
+            idlist.add(i);
+            j++;
+        }
+        System.out.println("------------------------");
+        return idlist;
+    }
+
+    @Override
+    public int update(Object obj) {
+
+        return 0;
+    }
+
+    public void updateItem() {
+
+        System.out.println("=> 수정할 단어 검색 : ");
+        String keyword = s.nextLine();
+
+        ArrayList<Integer> idlist = this.listAll(keyword); //?
+
+        System.out.println("=> 수정할 번호 선택 : ");
+        int num = s.nextInt();
+        s.nextLine(); //enter
+
+        System.out.println("뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        Word word = list.get(idlist.get(num - 1));
+        word.setMeaning(meaning);
+
+        System.out.println("단어가 수정되었습니다.");
+
+    }
+
+    @Override
+    public int delete(Object obj) {
+
+        return 0;
+    }
+
+    public void deleteItem() {
+
+        System.out.println("=> 삭제할 단어 검색 : ");
+        String keyword = s.nextLine();
+
+        ArrayList<Integer> idlist = this.listAll(keyword);
+
+        System.out.println("=> 삭제할 번호 선택 : ");
+        int num = s.nextInt();
+        s.nextLine(); //enter
+
+        System.out.println("=> 정말로 삭제하시겠습니까? (Y/N");
+        String ans = s.nextLine();
+
+        if (ans.equals("Y") || ans.equals("y")) {
+            list.remove((int) idlist.get(num - 1));
+            System.out.println("단어가 삭제되었습니다.");
+        } else if (ans.equals("N") || ans.equals("n")) {
+            System.out.println("취소되었습니다.");
+        }
+
+    }
+
+    @Override
+    public void selectOne(int id) {
+    }
 }
+
+/*
+    public void loadFile() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fname));
+            String line;
+            int count = 0;
+
+            while (true) {
+                line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+
+                String data[] = line.split ("\\|");
+                int level = Integer.parseInt(data[0]);
+                String word = data[1];
+                String meaning = data[2];
+
+                list.add(new Word (0, level,word,meaning));
+                count++;
+            }
+            br.close();
+            System.out.println("==>" + count + "개 로딩 완료");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+*/
